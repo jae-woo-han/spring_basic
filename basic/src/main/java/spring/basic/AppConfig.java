4ef8059e36205fadd3aec5abf99a4795ede893e9
jae-woo-han <40654123+jae-woo-han@users.noreply.github.com>
2021-04-28 22:13:13 +0900

    인스턴스 생성 분리

--- basic/src/main/java/spring/basic/AppConfig.java
+++ basic/src/main/java/spring/basic/AppConfig.java
@@ -1,6 +1,8 @@
 package spring.basic;
 
+import spring.basic.discount.DiscountPolicy;
 import spring.basic.discount.FixDiscountPolicy;
+import spring.basic.member.MemberRepository;
 import spring.basic.member.MemberService;
 import spring.basic.member.MemberServiceImpl;
 import spring.basic.member.MemoryMemberRepository;
@@ -10,12 +12,21 @@ import spring.basic.order.OrderServiceImpl;
 //의존성 주입을 해주는 클래스
 public class AppConfig {
 	
+	//객체 생성도 메서드로 분리해주는 것이 좋음
+	private MemberRepository memberRepository() {
+		return new MemoryMemberRepository();
+	}
+	
+	private DiscountPolicy discountPolicy() {
+		return new FixDiscountPolicy();
+	}
+	
 	//생성자 주입
 	public MemberService memberService() {
-		return new MemberServiceImpl(new MemoryMemberRepository());
+		return new MemberServiceImpl(memberRepository());
 	}
 
 	public OrderService orderService() {
-		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 }
